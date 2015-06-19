792e0b7605acc6b8e8caf073f49ea24ac31ec2e2
shekarsiri <sshekarsiri@gmail.com>
2015-06-19 12:01:21 +0530

    changed name space

---
+++ src/BaseRepo/RepositoryServiceProvider.php
@@ -0,0 +1,18 @@
+<?php namespace ShekarSiri\BaseRepo;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //dd('REPO');
+    }
+}
