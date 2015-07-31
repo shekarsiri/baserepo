2d4e9de6b122ac39139456868a86794e8eb5625b
shekarsiri <sshekarsiri@gmail.com>
2015-08-01 02:30:14 +0530

    added the whereHas to the pagination query

--- src/BaseRepo/PaginationTrait.php
+++ src/BaseRepo/PaginationTrait.php
@@ -12,29 +12,42 @@ trait PaginationTrait
         $model = $model->where(function ($q) use ($query) {
             foreach ($query as $key => $val) {
                 if ($val && array_key_exists($key, $this->queries) && $key != 'page') {
-                    switch ($this->queries[$key]) {
-                        case '=':
-                            $q->where($key, $val);
-                            break;
+                    
+                    $type = $this->queries[$key];
+                    if (!is_array($type)) {
+                        switch ($this->queries[$key]) {
+                            case '=':
+                                $q->where($key, $val);
+                                break;
 
-                        case 'like':
-                            $q->where($key, 'LIKE', '%' . $val . '%');
-                            break;
+                            case 'like':
+                                $q->where($key, 'LIKE', '%' . $val . '%');
+                                break;
 
-                        case 'in':
-                            $q->whereIn($key, $val);
-                            break;
+                            case 'in':
+                                $q->whereIn($key, $val);
+                                break;
 
-                        case 'whereHas':
-                            $q->whereHas($val[0], function ($q) use ($val) {
-                                $q->where($val[1], $val[2]);
-                            });
-                            break;
+                            case 'whereHas':
+                                $q->whereHas($val[0], function ($q) use ($val) {
+                                    $q->where($val[1], $val[2]);
+                                });
+                                break;
 
-                        default:
-                            $q->where($key, $val);
-                            break;
+                            default:
+                                $q->where($key, $val);
+                                break;
+                        }
+                    } else if (is_array($type)) {
+                        switch ($type['type']) {
+                            case 'whereHas':
+                                $q->whereHas($type['relation'], function ($q) use ($type, $val) {
+                                    $q->where($type['column'], $val);
+                                });
+                                break;
+                        }
                     }
+                    
                 }
             }
         });
